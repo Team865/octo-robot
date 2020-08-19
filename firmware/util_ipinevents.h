3bb6d15b39b28c7c17dbabb0f2b7006754b1e90a
Andrew Brownbill <andrew.brownbill@gmail.com>
2020-08-19 14:49:20 -0400

    Create a pipe data structure for timing Input pin events. This will be used, later, by the encoders driver, and probably the SR04 driver.

---
+++ firmware/util_ipinevents.h
@@ -0,0 +1,136 @@
+#ifndef __UTIL_IPINEVENTS_H__
+#define __UTIL_IPINEVENTS_H__
+
+#include <array>
+#include "hardware_interface.h"   // For HWI::PinState
+#include "time_types.h"           // For Time::DeviceTimeUS
+
+namespace Util {
+
+/// A single event for IPinEvents
+using IPinEvent = std::pair< HWI::PinState, Time::DeviceTimeUS >; 
+
+///
+/// @brief Record changes in an input, and the time the change occured
+///
+/// This class records changes that occur on a single GPIO pin, and the time
+/// the change occured at.
+///
+/// - Interrupt occurs because a GPIO pin changes state
+///   - An interrupt handler is called
+///   - The interrupt handler gets the time
+///   - The interrupt handler gets the current state of the pin
+///   - The interrupt handler records that information in IPinEvents  
+///   - The interrupt handler exits
+///
+/// Use Example:  Encoder
+/// 
+/// - The Encoder Command's execute() method is called
+///   - execute() looks at the IpinEvents for the two pins the encoder owns
+///   - execute() computes the current encoder state using the pin changes
+///   - execute() figures out the speed the encoder is moving from event times
+///
+/// The last job, "execute() figures out the speed the encoder is moving from
+/// from event times", is the reason we need the extra complexity of interrupt
+/// driven IO.  With polling we know that a pin state occured, but not 
+/// precisely when it occured.
+///
+/// Here is background reading on using interrupts:  https://randomnerdtutorials.com/interrupts-timers-esp8266-arduino-ide-nodemcu/
+///
+/// The guidance on that tutorial says that the interrupt should be quick and
+/// update a global variable.  That's more or less the approach that we'll
+/// take.  An IpinEvents will be a true singleton, in that there will only be
+/// a single instance for each class definition. 
+///
+/// @input[in] N   - the size of the event pipe
+///
+template< std::size_t N > 
+class IPinEvents
+{
+  public:
+
+  /// @brief Constructor
+  IPinEvents() :
+    writeSlot{ 0 },
+    readSlot{ 0 },
+    writeErrorFlag{ false },
+    readErrorFlag{ false }
+  {
+  }
+
+  /// @brief Write an event to the pipe.  Set error flag on overflow
+  /// 
+  void write( const IPinEvent& event ) noexcept {
+    // Find the next empty write slot
+    const std::size_t nextWriteSlot = ( writeSlot + 1 ) % N;
+
+    //
+    // An overflow will occur if advancing the writeSlot will move us to
+    // the readSlot - we're wrapping around in that case.  If that would
+    // happen, record nothing (no room), flag an error, and return.
+    //
+    if ( readSlot == nextWriteSlot ) 
+    {
+      writeErrorFlag = true;
+      return;
+    }
+
+    //
+    // Advance to the next write slot and record the event.
+    //
+    writeSlot = nextWriteSlot;
+    events[writeSlot] = event;
+  }
+
+  /// @brief Are there events available to read?
+  /// 
+  bool hasEvents() {
+    // If last slot where data was written is the same as the last slot we
+    // read data from.  There's no data in the pipe;
+    const bool noDataInPipe = ( readSlot == writeSlot );
+    return !noDataInPipe;
+  }
+
+  /// @brief Read an event to the pipe.  Set error flag if there are no events
+  /// 
+  IPinEvent read() {
+    if ( !hasEvents() ) {
+      // no events - never should have been called :(
+      readErrorFlag = true;
+      return IPinEvent();   // return an empty event - it's garbage anyway
+    }
+    readSlot = ( readSlot + 1 ) % N;
+    return events[ readSlot ];
+  }
+
+  /// @brief Did a read underflow error occur?
+  bool hasReadError()  const { return readErrorFlag;  }
+  /// @brief Did a write overflow error occur?
+  bool hasWriteError() const { return writeErrorFlag; }
+
+  private:
+  
+  // Index of the last slot where data was written
+  size_t writeSlot;
+  // Index of the last slot where data was read from
+  size_t readSlot;
+  // Set to true if we overflow on write
+  bool writeErrorFlag;
+  // Set to true if we underflow on read
+  bool readErrorFlag;
+  // The actual events.
+  std::array< IPinEvent, N > events;
+};
+} // End util namespace
+
+#ifdef TODO_SAMPLE_HANDLER
+
+  IpinEvents<16> pinEvents; 
+
+  void interrupt() {
+    pinEvents.write(IPinEvent( hwi->DigitalRead(pin) , hst->usSinceDeviceStart()));   
+  }
+#endif
+
+#endif
+
---
+++ unit_tests/test_ipinevents.cpp
@@ -0,0 +1,162 @@
+#include <gtest/gtest.h>
+
+/// 
+/// @brief Tests for Util::IPinEvents
+///
+
+#include "util_ipinevents.h"
+
+namespace Util {
+
+//
+// Repeatedly adds and removes events
+// 
+TEST( pipe_should, add_and_remove_events )
+{
+  IPinEvents<16> events;
+
+  const std::vector<IPinEvent> golden = {
+    { HWI::PinState::INPUT_HIGH, Time::DeviceTimeUS{1} },
+    { HWI::PinState::INPUT_LOW,  Time::DeviceTimeUS{2} },
+    { HWI::PinState::INPUT_HIGH, Time::DeviceTimeUS{3} }
+  };
+
+  // 100 loops should be a good stress test
+  for ( unsigned i = 0; i < 100; ++i )
+  {
+    // starts empty
+    ASSERT_EQ( false, events.hasEvents() );
+ 
+    // Add events
+    for ( const auto& event : golden ) 
+    {
+      events.write( event );
+    }
+
+    // Should no longer be empty
+    ASSERT_EQ( true,  events.hasEvents() );
+
+    // Remove events and make sure they're the right events
+    for ( const auto& event : golden ) 
+    {
+      ASSERT_EQ( event, events.read() );
+    }
+
+    // Tests.  Finishes empty & with no errors.
+    ASSERT_EQ( false, events.hasEvents() );
+    ASSERT_EQ( false, events.hasReadError() );
+    ASSERT_EQ( false, events.hasWriteError() );
+  }
+}
+
+//
+// Repeatedly write until the buffer is full and then drain
+//
+TEST( pipe_should, write_to_full )
+{
+  IPinEvents<4> events;
+
+  // Full is one less than the max (3) under the current implementation
+  //
+  const std::vector<IPinEvent> golden = {
+    { HWI::PinState::INPUT_HIGH, Time::DeviceTimeUS{1} },
+    { HWI::PinState::INPUT_LOW,  Time::DeviceTimeUS{2} },
+    { HWI::PinState::INPUT_HIGH, Time::DeviceTimeUS{3} }
+  };
+
+  // 100 = stress test
+  for( unsigned i = 0; i < 100; ++i ) {
+    // Starts empty
+    ASSERT_EQ( false, events.hasEvents() );
+ 
+    // Fill to max
+    for ( const auto& event : golden ) {
+      events.write( event );
+    }
+
+    // No longer empty
+    ASSERT_EQ( true,  events.hasEvents() );
+
+    // Read events
+    for ( const auto& event : golden ) {
+      ASSERT_EQ( event, events.read() );
+    }
+
+    // Should be empty & with no errors
+    ASSERT_EQ( false, events.hasEvents() );
+    ASSERT_EQ( false, events.hasReadError() );
+    ASSERT_EQ( false, events.hasWriteError() );
+  }
+}
+
+//
+// Overflow the write buffer and make sure the error flag is set
+//
+TEST( pipe_should, error_on_write_fail )
+{
+  IPinEvents<4> events;
+  
+  // Size 4 pipe can only hold 3 events, so feed it four events
+  const std::vector<IPinEvent> golden = {
+    { HWI::PinState::INPUT_HIGH, Time::DeviceTimeUS{1} },
+    { HWI::PinState::INPUT_HIGH, Time::DeviceTimeUS{2} },
+    { HWI::PinState::INPUT_LOW,  Time::DeviceTimeUS{3} },
+    { HWI::PinState::INPUT_LOW,  Time::DeviceTimeUS{4} }
+  };
+  for ( const auto& event : golden ) {
+    events.write( event );
+  }
+
+  // Should have error
+  ASSERT_EQ( true , events.hasWriteError() );
+
+  // First three events should be there - forth was lost.
+  for( std::size_t i = 0; i < 3; ++i ) {
+      ASSERT_EQ( golden[i], events.read());  
+  }
+
+  // Finishes with no events, but still has an error flag.
+  ASSERT_EQ( false, events.hasEvents() );
+  ASSERT_EQ( true , events.hasWriteError() );
+}
+
+//
+// Make sure the read flag is set if we try to read when there's no data
+//
+TEST( pipe_should, error_on_read_fail )
+{
+  IPinEvent event = 
+    { HWI::PinState::INPUT_LOW,  Time::DeviceTimeUS{1} };
+
+  // Go through some test permutations...
+  //
+  // prefeedCount:  How many events to add and remove at the beginning of the test
+  for ( std::size_t prefeedCount = 0; prefeedCount < 100; ++prefeedCount )
+  {
+    IPinEvents<15> events;
+
+    // Prefeed events
+    for ( std::size_t prefeed = 0; prefeed < prefeedCount; ++prefeed ) {
+      events.write( event );
+      ASSERT_EQ( event, events.read() );
+    }
+ 
+    // Should be empty and error free
+    ASSERT_EQ( false, events.hasEvents());
+    ASSERT_EQ( false, events.hasReadError());
+    ASSERT_EQ( false, events.hasWriteError());
+
+    // Do the illegal read.  rval is garbage.
+    events.read();
+
+    // should now have the read error flag set.
+    ASSERT_EQ( false, events.hasEvents());
+    ASSERT_EQ( true,  events.hasReadError());
+    ASSERT_EQ( false, events.hasWriteError());
+
+  }
+}
+
+
+} // end Util namespace
+
